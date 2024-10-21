3326878bff6aaa574f523ddcc289c3087cee788b
dayzintt <deividborges07@hotmail.com>
2024-10-21 10:34:34 -0300

    inicio do site

---
+++ assets/jss/jss.js
@@ -0,0 +1,30 @@
+//Index
+
+src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
+    integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n"
+    crossorigin="anonymous"
+
+src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
+    integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo"
+    crossorigin="anonymous"
+
+src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
+    integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6"
+    crossorigin="anonymous"
+
+
+function initMap() {
+    var map = L.map('map').setView([-22.948838252865173, -43.23465728802696], 15); // Coordenadas de São Paulo
+        
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+            
+    L.marker([-22.948838252865173, -43.23465728802696]).addTo(map)
+        .bindPopup('Endereço: Estrada da Pizzaria, 123456')
+        .openPopup();
+        }
+        
+    
+
+
